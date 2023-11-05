1464e532e0322e2b41f10eeb6adaf7ee6a9e02e7
chuxinyuan <314572603@qq.com>
2023-11-05 14:13:00 +0800

    update simplex

--- app.R
+++ app.R
@@ -16,75 +16,40 @@ local_repo = "../xmin"
 blog_url = "https://xmin.netlify.app"
 
 #------------------------------------------------------------------------------#
-#                              加载相关 R 包
-#------------------------------------------------------------------------------#
-
-library(shiny)
-library(shinydashboard)
-
-#------------------------------------------------------------------------------#
 #                             定义前端交互界面
 #------------------------------------------------------------------------------#
 
-ui = dashboardPage(
-
-  skin = "blue",
-  dashboardHeader(
-    title = "文件上传分享",
-    titleWidth = 260
-  ),
-
-  dashboardSidebar(
-    width = 260,
-    collapsed = TRUE,
-    sidebarMenu(
-      menuItem(
-        "开始使用",
-        tabName = "start",
-        icon = icon("rocket")
-      )
-    )
-  ),
-
-  dashboardBody(
-    tabItems(
-      tabItem(
-        tabName = "start",
-        fluidPage(
-          br(),
-          fluidPage(
-            fileInput(
-              inputId = "choose_file",
-              label = "选择上传文件",
-              buttonLabel = "浏览...",
-              placeholder = "没有选择文件",
-              width = "300px"
-            ),
-            actionButton(
-              inputId = "button_prepare",
-              label = "准备文件",
-              icon = icon("table"),
-              width = "300px",
-              class = "btn-primary btn-md"
-            ),
-            tableOutput("filelist")
-          ),
-          br(),
-          fluidPage(
-            actionButton(
-              inputId = "button_upload",
-              label = "上传文件",
-              width = "300px",
-              icon = icon("file-upload"),
-              class = "btn-primary btn-md"
-            ),
-            tableOutput("uploadfile")
+library(shiny)
+ui = fluidPage(
+  fluidRow(
+    column(
+      6, offset = 3,
+      br(),
+      br(),
+      br(),
+      wellPanel(
+        fileInput(
+          inputId = "choose_file",
+          label = "选择上传文件",
+          buttonLabel = "浏览...",
+          placeholder = "没有选择文件"
+        ),
+        div(
+          style = "display: inline-block;
+                   width: 100%;
+                   text-align: center;",
+          actionButton(
+            inputId = "button_upload",
+            label = "上传文件",
+            class = "btn-success",
           )
-        )
+        ),
+        br(),
+        tableOutput("uploadfile"),
+        br()
       )
     )
   )
-
 )
 
 #------------------------------------------------------------------------------#
@@ -93,41 +58,13 @@ ui = dashboardPage(
 
 server = function(input, output, session) {
   
-  # 创建对象来存储结果
-  values = reactiveValues()
-  
-  #----------------------------------------------------------------------------#
-  
-  # 准备文件
-  observeEvent(input$button_prepare, {
-    
-    ## 生成存放数据路径
-    current_time = format(Sys.time(), format = "%Y%m%d%H%M%S")
-    src = paste("./data", current_time, sep = "/")
-    if (!dir.exists(src)) dir.create(src, recursive = TRUE)
-    values$src = src
-    ## 选择上传文件后将文件放在制定的路径下
-    file.copy(
-      input$choose_file$datapath,
-      file.path(src, input$choose_file$name),
-      overwrite = TRUE
-    )
-    output$filelist = renderTable(
-      print(list.files(src, full.names = TRUE)),
-      colnames = FALSE
-    )
-    
-  })
-  
-  #----------------------------------------------------------------------------#
-  
   # 上传文件
   observeEvent(input$button_upload, {
     
     ## 定义要上传到 GitLab 上的文件
-    file = input$choose_file$name
-    src = values$src
-    file_from = file.path(src, file)
+    file = input$choose_file
+    file_name = file$name
+    file_path = file$datapath
     
     ## 把要上传的文件复制到本地指定目录下
     img_ext = c(
@@ -135,18 +72,18 @@ server = function(input, output, session) {
       "BMP", "svg", "SVG", "ico", "ICO", "tif", "TIF",
       "gif", "GIF", "webp", "WEBP", "tiff", "TIFF"
     )
-    if (tools::file_ext(input$choose_file$name) %in% img_ext) {
+    if (tools::file_ext(file_name) %in% img_ext) {
       dest_dir = "images"
     } else {
       dest_dir = "source"
     }
-    file_add = file.path(local_repo, "static", dest_dir, file)
-    file.copy(file_from, file_add)
+    file_add = file.path(local_repo, "static", dest_dir, file_name)
+    file.copy(file_path, file_add)
     Sys.sleep(2)
     
     ## 推送文件到 GitLab 指定仓库
     git2r::add(local_repo, file_add)
-    git2r::commit(local_repo, paste("upload", file, sep = " "))
+    git2r::commit(local_repo, paste("upload", file_name, sep = " "))
     git2r::push(
       object = local_repo,
       name = "origin",
@@ -156,7 +93,7 @@ server = function(input, output, session) {
     )
     
     ## 生成图片的 URL 地址
-    file_url = file.path(blog_url, dest_dir, file)
+    file_url = file.path(blog_url, dest_dir, file_name)
     
     ## 反馈文件的 URL
     output$uploadfile = renderTable(
